8ad1fb910ab86ead4690f38373d94b755837f074
mrbaloch555 <mehello456@gmail.com>
2023-05-06 16:47:32 +0500

    new changes

--- Models/Getter/RegisterModel.js
+++ Models/Getter/RegisterModel.js
@@ -1,91 +1,97 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-const RegisterModel = mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    
-    lastName:{
-        type:String,
-        required:true
+const RegisterModel = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
 
-    email:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
     },
 
-    password:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
     },
 
-    username:{
-        type:String,
-        default:null
+    password: {
+      type: String,
+      required: true,
     },
 
-    phonenumber:{
-        type:String,
-        default:null,
+    username: {
+      type: String,
+      default: null,
     },
 
-    credit:{
-        type:Number
+    phonenumber: {
+      type: String,
+      default: null,
     },
 
-    dateOfBirth:{
-        type:String,
-        default:null
+    credit: {
+      type: Number,
     },
 
-    gender:{
-        type:String,
-        default:null
+    dateOfBirth: {
+      type: String,
+      default: null,
     },
 
-    country:{
-        type:String,
-        default:null
+    gender: {
+      type: String,
+      default: null,
     },
 
-    profileImage:{
-        type:String,
-        default:null
+    country: {
+      type: String,
+      default: null,
     },
-    OTP:{
-        type:Number,
-        default:null
-    },
-    otpValidTill:{
-        type:Date,
-        default:null,
-    },
-    otpVerified:{
-        type:Boolean,
-        default:false
-    },
-    accountBlocked:{
-        type:Boolean,
-        default:false
-    },
-    accountMuted:{
-        type:Boolean,
-        default:false
-    }
-    
-},{
-    toJSON:{
-        transform(doct, ret) {
-            delete ret.password;
-            delete ret.createdAt;
-            delete ret.updatedAt;
-        }
-    },timestamps: true,
-}
-)
 
-const GetterInfo = mongoose.model('GetterInfo',RegisterModel,'GetterInfo')
+    profileImage: {
+      type: String,
+      default: null,
+    },
+    OTP: {
+      type: Number,
+      default: null,
+    },
+    otpValidTill: {
+      type: Date,
+      default: null,
+    },
+    otpVerified: {
+      type: Boolean,
+      default: false,
+    },
+    accountBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    accountMuted: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      default: "Getter",
+    },
+  },
+  {
+    toJSON: {
+      transform(doct, ret) {
+        delete ret.password;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
 
-module.exports = GetterInfo
+const GetterInfo = mongoose.model("GetterInfo", RegisterModel, "GetterInfo");
+
+module.exports = GetterInfo;
--- Models/Setter/RegisterModel.js
+++ Models/Setter/RegisterModel.js
@@ -1,81 +1,87 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-const RegisterModel = mongoose.Schema({
-    
-    firstName:{
-        type:String,
-        required:true
+const RegisterModel = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    lastName:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
 
-    username:{
-        type:String,
-        default:null
+    username: {
+      type: String,
+      default: null,
     },
-    phonenumber:{
-        type:String,
-        default:null,
+    phonenumber: {
+      type: String,
+      default: null,
     },
-    credit:{
-        type:Number
+    credit: {
+      type: Number,
     },
-    dateOfBirth:{
-        type:String,
-        default:null
+    dateOfBirth: {
+      type: String,
+      default: null,
     },
-    gender:{
-        type:String,
-        default:null
+    gender: {
+      type: String,
+      default: null,
     },
-    country:{
-        type:String,
-        default:null
+    country: {
+      type: String,
+      default: null,
     },
-    profileImage:{
-        type:String,
-        default:null
+    profileImage: {
+      type: String,
+      default: null,
     },
-    OTP:{
-        type:Number,
-        default:null
+    OTP: {
+      type: Number,
+      default: null,
     },
-    otpValidTill:{
-        type:Date,
-        default:null,
+    otpValidTill: {
+      type: Date,
+      default: null,
     },
-    otpVerified:{
-        type:Boolean,
-        default:false
+    otpVerified: {
+      type: Boolean,
+      default: false,
     },
-    accountBlocked:{
-        type:Boolean,
-        default:false
+    accountBlocked: {
+      type: Boolean,
+      default: false,
     },
-    accountMuted:{
-        type:Boolean,
-        default:false
-    }
-},{
-    toJSON:{
-        transform(doct, ret) {
-            delete ret.password;
-            delete ret.createdAt;
-            delete ret.updatedAt;
-        }
-    },timestamps: true,
-}
-)
-const SetterInfo = mongoose.model('SetterInfo',RegisterModel,'SetterInfo')
+    accountMuted: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      default: "Setter",
+    },
+  },
+  {
+    toJSON: {
+      transform(doct, ret) {
+        delete ret.password;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
+const SetterInfo = mongoose.model("SetterInfo", RegisterModel, "SetterInfo");
 
-module.exports = SetterInfo
+module.exports = SetterInfo;
