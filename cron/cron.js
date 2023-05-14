6f8adf31f6d10b865af9b96b09016d164d409991
Talha <talhahaider074@gmail.com>
2023-05-14 17:09:18 +0500

    apis updated

--- cron/cron.js
+++ cron/cron.js
@@ -2,7 +2,7 @@ const { GameModel } = require("../Models");
 const cron = require("node-cron");
 
 const gameTimer = () => {
-  cron.schedule("*/1 * * * *", async () => {
+  cron.schedule("* * * * * *", async () => {
     let games = await GameModel.find({ active: true });
     console.log("running");
     if (games.length) {
@@ -22,25 +22,8 @@ const gameTimer = () => {
         await games[i].save();
       }
     }
-    // if (findGame.length > 0) {
-    //   findGame.forEach((game) => {
-    //     if (game.duration.sec > 0) {
-    //       game.duration.sec--;
-    //     } else if (game.duration.min > 0) {
-    //       game.duration.sec = 59;
-    //       game.duration.min--;
-    //     } else if (game.duration.hours > 0) {
-    //       game.duration.sec = 59;
-    //       game.duration.min = 59;
-    //       game.duration.hours--;
-    //     } else {
-    //       // All duration fields are zero, so set active to false
-    //       game.active = false;
-    //     }
-    //     game.save();
-    //   });
-    // }
   });
 };
+
 
 module.exports = { gameTimer };
